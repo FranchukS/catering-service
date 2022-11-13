aa33a55a817994fda03ae122ccf305bff3891510
Sergii Franchuk <Franchuksergei@gmail.com>
2022-11-13 16:15:20 +0200

    Add extra validation for years_of_experiece fiels

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,12 +1,32 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from kitchen.models import Cook
 
 
-class CookCreationForm(UserCreationForm):
+class ExperienceValidateMixin:
+    def clean_years_of_experience(self):
+        years_of_experience = self.cleaned_data["years_of_experience"]
+
+        if years_of_experience < 0:
+            raise ValidationError(
+                "Experience can't be lower then 0"
+            )
+
+        if years_of_experience > 50:
+            raise ValidationError(
+                "Invalid input max value is 50"
+            )
+
+        return years_of_experience
+
+
+class CookCreationForm(ExperienceValidateMixin, UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = Cook
         fields = UserCreationForm.Meta.fields + (
             "years_of_experience", "email", "first_name", "last_name",
         )
+
+
