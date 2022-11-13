0920fee46dccc5a87d4d8d36c97a598702556ba6
Sergii Franchuk <Franchuksergei@gmail.com>
2022-11-13 02:07:13 +0200

    Add view and template for index page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Cook, Dish, Category
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_categories = Category.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_categories": num_categories,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
