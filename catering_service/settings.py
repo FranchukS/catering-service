a7dad61f2d368b87f28ae4b143a57b50c3fa2172
Sergii Franchuk <Franchuksergei@gmail.com>
2022-11-16 21:24:22 +0200

    Add catering-service.onrender.com to ALLOWED_HOSTS

--- catering_service/settings.py
+++ catering_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "catering-service.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
