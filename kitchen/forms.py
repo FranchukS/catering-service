50ddb9da7d884c8278428069025a5b5be0c74937
Sergii Franchuk <Franchuksergei@gmail.com>
2022-12-04 23:05:41 +0200

    Add new search form for cook with placeholder by username

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -68,3 +68,12 @@ class SearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={"placeholder": "Search by name..."})
     )
+
+
+class UserSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username..."})
+    )
